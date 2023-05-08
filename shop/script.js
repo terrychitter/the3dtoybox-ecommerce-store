f774f33fd3b1cf4610c7b2be32b6307624d269a0
Terry <chittert@gmail.com>
2023-05-08 17:40:33 +0200

    add autmatic switch in promotional slides

--- shop/script.js
+++ shop/script.js
@@ -1,19 +1,36 @@
 const buttons = document.querySelectorAll("[data-carousel-button]");
+const intervalTime = 5000; // 5 seconds
+
+// Call the promotionalSlideNext function every 5 seconds
+const intervalId = setInterval(() => {
+  promotionalSlideNext(buttons[0]);
+}, intervalTime);
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
+    promotionalSlideNext(button);
+  });
+});
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+function promotionalSlideNext(button) {
+  const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+  const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
 
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+  const activeSlide = slides.querySelector("[data-active]");
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
 
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
+
+// Stop the automatic slide change when the user clicks a button
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    clearInterval(intervalId);
   });
 });
