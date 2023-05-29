2bd586ef187ef5f497825a6d57307d957e85fd2d
Terry <chittert@gmail.com>
2023-05-30 01:19:40 +0200

    redesign home page

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!-- https://imgbox.com/gallery/edit/7d4eje7GLA/mFJBwEhAGY8B56Dw (Images link) -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -6,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="style.css" />
+    <script src="script.js" defer></script>
     <script src="removeParams.js"></script>
     <script
       src="https://kit.fontawesome.com/805e2252e6.js"
@@ -16,7 +16,40 @@
   <body>
     <?php include "header/header.php";
     include "popup/popup.php";?>
-    <section class="hero"></section>
+            <div id="promotions" class="promotions" data-carousel>
+          <button class="carousel-button prev" data-carousel-button="prev">
+              <i class="fa-solid fa-circle-chevron-left fa-xl"></i></i>
+          </button>
+          <button class="carousel-button next" data-carousel-button="next">
+            <i class="fa-solid fa-circle-chevron-right fa-xl"></i>
+          </button>
+          <div class="carousel">
+            <ul data-slides>
+              <li class="promotional-slide" data-active>
+                <img
+                  src="https://media.discordapp.net/attachments/1085644871508971680/1112797089534062643/1.png?width=790&height=395"
+                  alt=""
+                  class="promotion-img"
+                />
+              </li>
+              <li class="promotional-slide">
+                <img
+                  src="https://media.discordapp.net/attachments/1085644871508971680/1112797090389696562/2.png?width=790&height=395"
+                  alt=""
+                  class="promotion-img"
+                />
+              </li>
+              <li class="promotional-slide">
+                <img
+                  src="https://media.discordapp.net/attachments/1085644871508971680/1112797091245338706/3.png?width=790&height=395"
+                  alt=""
+                  class="promotion-img"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
+    <main>
     <section class="categories">
       <h2>Search Categories</h2>
       <div class="categories-grid">
@@ -28,241 +61,65 @@
         </button>
       </div>
     </section>
-    <div class="featured-new-container">
-      <section class="featured-products">
-        <h2>Featured Products</h2>
-        <div class="product-scroller">
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-        </div>
-        <button class="see-more">See More<i class="fa-solid fa-angles-right"></i></button>
+    <section class="featured">
+      <h2>Featured Products</h2>
+            <ul class="scroller product-scroller">
+            <?php // Getting all featured products
+            include "db_conn.php";
+              $sql = "SELECT * FROM products WHERE featured=true";
+              $result = mysqli_query($conn, $sql);
+              
+              while ($row = mysqli_fetch_assoc($result)) { ?>
+              <li class="product-card" data-productId="<?php echo $row['product_id'] ?>">
+                <img src="../product_images/<?php echo $row['product_id']?>.png" loading="lazy" alt="<?php echo $row['name']; ?>">
+                <div class="add-to-wishlist">
+                <i class="<?php if (isset($_SESSION['wishlist'])) {
+                    if (in_array($row['product_id'], $_SESSION['wishlist'])) {
+                      ?> fa-solid <?php } else  { echo "fa-regular"; }
+                      } else {?>fa-regular <?php } ?>
+                      fa-heart"></i>
+                </div>
+                <p class="product-name"><?php echo $row['name']; ?></p>
+                <p class="product-desc">
+                  <?php echo $row['description']; ?>
+                </p>
+                <p class="product-price">R 
+                  <?php echo $row['price']; ?>
+                </p>
+                <button class="add-to-cart">Add to Cart</button>
+              </li><?php } ?>
+            </ul>
+    </section>
+      <section class="new">
+      <h2>New Products</h2>
+          <ul class="scroller product-scroller">
+            <?php
+            // Getting all new products
+            $sql = "SELECT * FROM products WHERE new=true";
+            $result = mysqli_query($conn, $sql);
+            
+            while ($row = mysqli_fetch_assoc($result)) { ?>
+            <li class="product-card" data-productId="<?php echo $row['product_id'] ?>">
+              <img src="../product_images/<?php echo $row['product_id']?>.png" loading="lazy" alt="<?php echo $row['name'];?>">
+              <div class="add-to-wishlist">
+                <i class="<?php if (isset($_SESSION['wishlist'])) {
+                  if (in_array($row['product_id'], $_SESSION['wishlist'])) {
+                    ?> fa-solid <?php } else  { echo "fa-regular"; }
+                    } else {?>fa-regular <?php } ?>
+                    fa-heart"></i>
+              </div>
+              <p class="product-name"><?php echo $row['name']; ?></p>
+              <p class="product-desc">
+                <?php echo $row['description']; ?>
+              </p>
+              <p class="product-price">R 
+                <?php echo $row['price']; ?>
+              </p>
+              <button class="add-to-cart">Add to Cart</button>
+            </li><?php } ?>
+          </ul>
       </section>
-      <section class="new-products">
-        <h2>New Products</h2>
-        <div class="product-scroller">
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-          <div class="product-card">
-            <img src="https://media.discordapp.net/attachments/1085644871508971680/1097870354766114837/terryc_2_cute_cats_sitting_innocently_cheeks_full_of_food_larfe_9716cf63-74de-4603-922a-b24273e474e0.png?width=677&height=677" alt="product-image">
-            <div class="add-to-wishlist">
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="product-label">
-              New
-            </div>
-            <p class="product-name">Product Name</p>
-            <p class="product-desc">
-              This is the product description
-            </p>
-            <p class="product-price">
-              R299.99
-            </p>
-            <button class="add-to-cart">Add to Cart</button>
-          </div>
-        </div>
-        <button class="see-more">See More<i class="fa-solid fa-angles-right"></i></button>
-      </section>
-    </div>
+    </main>
     <?php include "footer/footer.php" ?>
   </body>
 </html>
--- script.js
+++ script.js
@@ -1,14 +1,87 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+//ADDING ITEMS TO THE WISHLIST
+// Get all the product cards
+console.log("Bottom script loaded");
+const productCards = document.querySelectorAll(".product-card");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+// Loop through each product card
+productCards.forEach((productCard) => {
+  // Find the heart icon element
+  const heartIcon = productCard.querySelector(".fa-heart");
+
+  // Get the data-productId value
+  const productId = productCard.getAttribute("data-productId");
+
+  // Check the initial class of the heart icon
+  const isRegularHeart = heartIcon.classList.contains("fa-regular");
+
+  // Add a click event listener to the product card
+  productCard.addEventListener("click", () => {
+    // Navigate to the product page with the corresponding product ID
+    window.location.href = "product/product.php?product_id=" + productId;
+  });
+
+  // Add a click event listener to the heart icon
+  heartIcon.addEventListener("click", (event) => {
+    event.stopPropagation(); // Stop event propagation
+
+    // Check the updated class of the heart icon
+    const isRegularHeartNow = heartIcon.classList.contains("fa-regular");
+
+    // Add or remove the productId from the wishlist array
+    if (isRegularHeartNow) {
+      // Add productId to the wishlist
+      console.log("Added to Wishlist");
+      heartIcon.classList.remove("fa-regular");
+      heartIcon.classList.add("fa-solid");
+      window.location.href = "add_item_wishlist.php?product_id=" + productId;
+    } else {
+      // Remove productId from the wishlist
+      heartIcon.classList.remove("fa-solid");
+      heartIcon.classList.add("fa-regular");
+      window.location.href = "remove_item_wishlist.php?product_id=" + productId;
+    }
+  });
+
+  // Get all the add-to-cart buttons
+  const addToCartButtons = productCard.querySelectorAll(".add-to-cart");
+  // Attach an event listener to each of the add to cart button
+  addToCartButtons.forEach((addToCartButton) => {
+    addToCartButton.addEventListener("click", (event) => {
+      event.stopPropagation(); // Stop event propagation
+
+      // Use the captured productId from the closure
+      const clickedProductId = productId;
+      window.location.href = "add_item_cart.php?product_id=" + clickedProductId;
+    });
+  });
 });
 
-addEventListener("scroll", () => {
-  if (hamburger.classList.contains("active")) {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  }
+const buttons = document.querySelectorAll("[data-carousel-button]");
+const intervalTime = 5000; // 5 seconds
+
+// Call the promotionalSlideNext function every 5 seconds
+const intervalId = setInterval(() => {
+  promotionalSlideNext(buttons[0]);
+}, intervalTime);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    promotionalSlideNext(button);
+  });
 });
+
+function promotionalSlideNext(button) {
+  const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+  const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
+
+  const activeSlide = slides.querySelector("[data-active]");
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+}
